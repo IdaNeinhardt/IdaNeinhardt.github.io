ba95aa1af864debd2f827a49204a8513cfb5bb66
Ida L. Neinhardt <ida.neinhardt@gmail.com>
2024-11-03 11:26:39 +0100

    created navigation bar

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def serve():
 def home():
     return render_template("home.html")
 
-#@app.route("/<page>")
-#def pages(page):
-#    return render_template("/" + page.lower() + ".html")
+@app.route("/<page>")
+def pages(page):
+    return render_template("/" + page.lower())
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
