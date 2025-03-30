99231f67b8f366ff127ffacbba4dfe619c72600d
IdaNeinhardt <ida.neinhardt@gmail.com>
2025-03-30 17:15:08 +0200

    updated map sizing

--- static/world-map.js
+++ static/world-map.js
@@ -1,36 +1,41 @@
 google.charts.load('current', {
-        'packages':['geochart'],
-      });
-      google.charts.setOnLoadCallback(drawRegionsMap);
+    'packages': ['geochart'],
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
 
-      function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable([
-          ['Country', 'Years', { role: 'tooltip' }],
-          ['Sweden', 1, "2001-2018, 2019-2023"],
-          ['France', 1, "2018-2019"],
-          ['Switzerland', 1, "2023-"]
-        ]);
+let chart, data;
 
-        var options = {
-            region: "150", // Specify Europe for region
-            legend: "none",
-            backgroundColor: "#A9C7CD",
-            colorAxis: {colors: "#6f8b9e"}
-        };
+function drawRegionsMap() {
+    data = google.visualization.arrayToDataTable([
+        ['Country', 'Years', { role: 'tooltip' }],
+        ['Sweden', 1, "2001-2018, 2019-2023"],
+        ['France', 1, "2018-2019"],
+        ['Switzerland', 1, "2023"]
+    ]);
 
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    var container = document.getElementById('regions_div_container');
+    var options = {
+        region: "150",
+        legend: "none",
+        backgroundColor: "#A9C7CD",
+        colorAxis: { colors: ["#6f8b9e"] },
+        keepAspectRatio: true,
+        width: container.clientWidth,
+        height: container.clientHeight
+    };
 
-        chart.draw(data, options);
+    chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    chart.draw(data, options);
 
-        google.visualization.events.addListener(chart, 'select', function() {
+    google.visualization.events.addListener(chart, 'select', function() {
             var selection = chart.getSelection();
             if (selection.length > 0) {
               var selectedCountry = data.getValue(selection[0].row, 0);
   
               var urls = {
-                'Sweden': 'projects/FSP.html',
-                'France': 'projects/FSP.html',
-                'Switzerland': 'projects/FSP.html'
+                'Sweden': 'map.html#Sweden',
+                'France': 'map.html#France',
+                'Switzerland': 'map.html#Switzerland'
               };
   
               if (urls[selectedCountry]) {
@@ -38,5 +43,7 @@ google.charts.load('current', {
               }
             }
           });
-      }
-      
+}
+
+// Redraw map when window is resized
+window.addEventListener('resize', drawRegionsMap);
